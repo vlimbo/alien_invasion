ea8679e0077d6914aff1e97c5b42e0bfcebff0cc
Vetle Limbodal <vwlimbo@proton.me>
2024-11-23 12:01:59 +0100

    Add ship instance to main game class to load in loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -14,6 +15,10 @@ class AlienInvasion:
         # Set the screen size of the game
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        # Creating an instance of ship class
+        # Passing an instance of the AlienInvasion to the ship class
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -21,6 +26,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
 
