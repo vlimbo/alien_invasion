90a5efea0bf61167e645662cfe906c18dbe1ad29
Vetle Limbodal <vwlimbo@proton.me>
2024-11-23 11:15:11 +0100

    Add bg color property and to main game loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,12 +10,17 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set background-color property
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
