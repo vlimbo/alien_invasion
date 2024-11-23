bf40424c0eaf4a90b074dc5f6375b92308ae3c8b
Vetle Limbodal <vwlimbo@proton.me>
2024-11-23 11:33:20 +0100

    Add settings.py with settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,26 +1,26 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         # Initialize the background settings
         pygame.init()
 
-        # Set the screen size of the game
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
         # Creating a clock to make sure we get consistent fps
         self.clock = pygame.time.Clock() # Again this creates an object
+        self.settings = Settings()
 
-        # Set background-color property
-        self.bg_color = (230, 230, 230)
+        # Set the screen size of the game
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
             self.clock.tick(60)
 
